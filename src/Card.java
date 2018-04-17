751f30ac49a1e9537301bea3d4b5494cc579875e
Owner <Owner@104.39.220.9>
2018-04-17 14:49:14 -0400

    Created Card Class

--- src/Card.java
+++ src/Card.java
@@ -9,5 +9,34 @@
  * @author angie
  */
 public class Card {
+    private String suit;
+    private String symbol;
+   
+    public Card(String suit, String symbol) {
+        this.suit = suit;
+        this.symbol = symbol;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public String toString() {
+        return "Suit: " + this.suit + ", Symbol" + this.symbol;
+    }
+    
     
 }
