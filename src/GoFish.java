6c6f5e922a184693f45208a6cc8add4a6694fbc1
Angie DeMarco <angie.demarco98@gmail.com>
2018-04-19 14:23:17 -0400

    updated constructor to deal hands

--- src/GoFish.java
+++ src/GoFish.java
@@ -1,5 +1,4 @@
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /*
@@ -7,20 +6,33 @@ import java.util.ArrayList;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author angie
  */
 public class GoFish {
+
     int score;
     ArrayList<Card> compHand = new ArrayList();
     ArrayList<Card> userHand = new ArrayList();
     Deck deck;
 
     public GoFish() {
-        this.score = score;
-        this.deck = deck;
+        this.score = 0;
+        this.deck = new Deck();
+        int numPlayers = 2;
+        int numStartingCards = 7;
+        Card[][] cardsDealt = deck.dealHands(numPlayers, numStartingCards);
+        for (int i = 0; i < numPlayers; i++) {
+            for (int j = 0; j < numStartingCards; j++) {
+                if (i==0){
+                    compHand.add(cardsDealt[i][j]);
+                }
+                else{
+                    userHand.add(cardsDealt[i][j]);
+                }
+            }
+        }
     }
 
     public int getScore() {
@@ -54,15 +66,9 @@ public class GoFish {
     public void setDeck(Deck deck) {
         this.deck = deck;
     }
-    
-   
-  public int updateScore(){
-      return 0;
-  }  
-    
-    
-    
+
+    public int updateScore() {
+        return 0;
+    }
+
 }
-
-
-
