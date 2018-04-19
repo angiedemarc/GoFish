03f1a70dbb96f02839caee153d07a34cb77b8074
Owner <Owner@104.39.220.9>
2018-04-19 13:54:20 -0400

    Started Go Fish class

--- src/GoFish.java
+++ src/GoFish.java
@@ -1,3 +1,7 @@
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +13,56 @@
  * @author angie
  */
 public class GoFish {
+    int score;
+    ArrayList<Card> compHand = new ArrayList();
+    ArrayList<Card> userHand = new ArrayList();
+    Deck deck;
+
+    public GoFish(int score, Deck deck) {
+        this.score = score;
+        this.deck = deck;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public ArrayList<Card> getCompHand() {
+        return compHand;
+    }
+
+    public ArrayList<Card> getUserHand() {
+        return userHand;
+    }
+
+    public Deck getDeck() {
+        return deck;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setCompHand(ArrayList<Card> compHand) {
+        this.compHand = compHand;
+    }
+
+    public void setUserHand(ArrayList<Card> userHand) {
+        this.userHand = userHand;
+    }
+
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+    }
+    
+   
+  public int updateScore(){
+      return 0;
+  }  
+    
+    
     
 }
+
+
+
